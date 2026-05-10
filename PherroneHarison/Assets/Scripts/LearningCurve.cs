b1fac56230969ed197887a7b45ef5593e0808008
Алексей <manay.ru@mail.ru>
2026-05-10 13:28:49 +1000

    Форматированный вывод на экран

--- PherroneHarison/Assets/Scripts/LearningCurve.cs
+++ PherroneHarison/Assets/Scripts/LearningCurve.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class LearningCurve : MonoBehaviour
 {
-    public int currentAge = 30;
+    
+    private int currentAge = 30;
+
+    [Header("Varieble")]
     public int addedAge = 1;
+    public float pi = 3.14f;
+    public string firstName = "Harisson";
+    public double money = 12.5d;
+    public bool isAuthor = true;
 
     
     void Start()
     {
         ComputeAdge();
+        Debug.LogFormat($"Varieble {addedAge} {pi} {firstName} {money}");
     }
 
     // Update is called once per frame
